33172860b70a5647bdad9ecf50372bb82e4e227c
pdhan780 <100167525+pdhan780@users.noreply.github.com>
2024-03-16 15:08:59 -0600

    small comment for clarity

--- src/components/LoginContainer.jsx
+++ src/components/LoginContainer.jsx
@@ -3,7 +3,8 @@ import VideoBackground from './VideoBackground';
 import Footer from "./Footer";
 import LoginPage from "./LoginPage";
 import Header from "./Header"; // Assuming you have created this
-
+//LoginContainer Serves as the main component holding all child components you see 
+//such as the login form, video, and header/footer
 const LoginContainer = () => {
     return (
       <div className="flex flex-col min-h-screen">
