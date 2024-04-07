fe154580dad38c52153ee32415b8bd391b008cef
pdhan780 <100167525+pdhan780@users.noreply.github.com>
2024-04-06 18:30:57 -0600

    added link to favorites-drivers

--- src/components/HomeViews/HomeViewQualifying/QualifyingList.jsx
+++ src/components/HomeViews/HomeViewQualifying/QualifyingList.jsx
@@ -10,6 +10,10 @@ import QualifyingItem from "./QualifyingItem";
 
   // Parent List component for Drivers
 const QualifyingList = ({qualifying}) => {  
+  const handleAddToFavorites = (name, driverProfile) => {
+    // Handle adding to favorites here
+    console.log("Adding to favorites with QLIST:", name, driverProfile);
+  };
     return (
         <div className="overflow-y-scroll max-h-custom1 scrollbar">
           <List className="[&>*:nth-child(odd)]:bg-f1-light-gray [&>*:nth-child(even)]:bg-white p-0 m-0">
