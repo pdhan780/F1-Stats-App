7c73a587a21a187121bffd669134a326ad4db7f0
bpear520 <bpear520@mtroyal.ca>
2024-04-07 13:33:25 -0600

    Formatted Podium a little

--- src/components/HomeViews/HomeViewResults/PodiumPlace.jsx
+++ src/components/HomeViews/HomeViewResults/PodiumPlace.jsx
@@ -1,8 +1,14 @@
 const PodiumPlace = ({ result, backgroundHeight, positionOrder, imageWidth, imageHeight }) => {
     if (!result) return null; // If there's no result, don't render anything
+    const getSlashesForPosition = (position) => {
+      return '/'.repeat(position);
+    };
+
+    
+  const slashes = getSlashesForPosition(positionOrder);
   
     return (
-      <div className="flex flex-col items-center mx-2 relative">
+      <div className="flex flex-col items-center mx-1 relative hover:-translate-y-2 hover:scale-105 transition ease-in-out delay-25">
         <div className={`winner_photo_bg rounded-t-lg rounded-br-lg absolute bottom-0 ${backgroundHeight} ${imageWidth}`}></div>
         <img
           className={`${imageWidth} ${imageHeight} relative z-10`}
@@ -10,8 +16,7 @@ const PodiumPlace = ({ result, backgroundHeight, positionOrder, imageWidth, imag
           alt={`${result.drivers.forename} ${result.drivers.surname}`}
         />
         <div className="z-10 py-1 relative bg-white w-full h-full rounded-br-lg border-f1-black border-r border-b hover:border-candy-apple">
-          <h1 className="font-f1 font-bold text-center">
-            {positionOrder}{result.drivers.code}
+          <h1 className="font-f1 text-lg font-bold text-center"><div className="text-xl text-candy-apple inline-block pr-2">{slashes}</div>{result.drivers.code}
           </h1>
         </div>
       </div>
--- src/components/HomeViews/HomeViewResults/ResultsPodium.jsx
+++ src/components/HomeViews/HomeViewResults/ResultsPodium.jsx
@@ -1,20 +1,44 @@
 import PodiumPlace from "./PodiumPlace";
 
 const ResultsPodium = ({ results }) => {
-    const backgroundHeights = ["h-28", "h-24", "h-20"]; // Heights for 1st, 2nd, and 3rd places
-    const imageWidth = ["w-28", "w-24"]; // Sizes for 1st and 2nd/3rd places
-    const imageHeight = ["h-28", "h-24"]; // Sizes for 1st and 2nd/3rd places
-  
-    // Only render if we have results
-    if (results.length < 3) return null;
-  
-    return (
-      <div className="flex justify-center items-end pt-4 space-x-4">
-        <PodiumPlace result={results[1]} backgroundHeight={backgroundHeights[1]} positionOrder="2" imageWidth={imageWidth[1]} imageHeight={imageHeight[1]} />
-        <PodiumPlace result={results[0]} backgroundHeight={backgroundHeights[0]} positionOrder="1" imageWidth={imageWidth[0]} imageHeight={imageHeight[0]}/>
-        <PodiumPlace result={results[2]} backgroundHeight={backgroundHeights[2]} positionOrder="3" imageWidth={imageWidth[1]} imageHeight={imageHeight[1]}/>
+  const backgroundHeights = ["h-28", "h-24", "h-20"]; // Heights for 1st, 2nd, and 3rd places
+  const imageWidth = ["w-28", "w-24"]; // Sizes for 1st and 2nd/3rd places
+  const imageHeight = ["h-28", "h-24"]; // Sizes for 1st and 2nd/3rd places
+
+  // Only render if we have results
+  if (results.length < 3) return null;
+
+  return (
+    <div className="flex border-b-4 border-f1-black ">
+      <div className="flex-1">
       </div>
-    );
-  };
-  
-  export default ResultsPodium;
+      <div className="flex-grow flex justify-center items-end py-4 space-x-10 ">
+        <PodiumPlace
+          result={results[1]}
+          backgroundHeight={backgroundHeights[1]}
+          positionOrder="2"
+          imageWidth={imageWidth[1]}
+          imageHeight={imageHeight[1]}
+        />
+        <PodiumPlace
+          result={results[0]}
+          backgroundHeight={backgroundHeights[0]}
+          positionOrder="1"
+          imageWidth={imageWidth[0]}
+          imageHeight={imageHeight[0]}
+        />
+        <PodiumPlace
+          result={results[2]}
+          backgroundHeight={backgroundHeights[2]}
+          positionOrder="3"
+          imageWidth={imageWidth[1]}
+          imageHeight={imageHeight[1]}
+        />
+      </div>
+      <div className="flex-1">
+      </div>
+    </div>
+  );
+};
+
+export default ResultsPodium;
