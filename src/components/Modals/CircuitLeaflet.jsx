958bbd6cd3e352ed4f0e9b5a90daaa433ebc0c6d
Pelvain Dhanda <100167525+pdhan780@users.noreply.github.com>
2024-06-18 16:01:58 -0600

    Update CircuitLeaflet.jsx

--- src/components/Modals/CircuitLeaflet.jsx
+++ src/components/Modals/CircuitLeaflet.jsx
@@ -1,7 +1,11 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; // Make sure to import the Leaflet CSS
 
-
+import { Icon } from 'leaflet'
+const myIcon = new Icon({
+ iconUrl: "/assets/marker.png",
+ iconSize: [25,25]
+})
 const CircuitLeaflet = ({ lat, lng, name }) => {
     const position = [lat, lng];
 
@@ -11,7 +15,7 @@ const CircuitLeaflet = ({ lat, lng, name }) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={position} icon={myIcon}> 
           <Popup>
             {name}
           </Popup>
